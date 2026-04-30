30bfea0207cd8b28e2adaf04dcb09e15461daef0
yara22aulia <yara22aulia@gmail.com>
2026-04-30 21:44:44 +0700

    validasi data duplikat pada node pertama

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -1,21 +1,7 @@
-// 4NimBelakang_DoubleLinkedList
-#include <iostream>
-#include <string>
-using namespace std;
-
-class Node
-{
-public:
-    int noMhs;
-    Node *next;
-    Node *prev;
-};
-
 class DoubleLinkedList
 {
 private:
     Node *START;
-}
 
 public:
     DoubleLinkedList()
@@ -23,9 +9,15 @@ public:
         START = NULL;
     }
 
-void addNode()
+    void addNode()
     {
         int nim;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+
+        //Step 1 Allocate memory for new node
+        Node *newNode = new Node();
+
+        //Step 2 Assign value to the data fields
+        newNode->noMhs = nim;
     }
